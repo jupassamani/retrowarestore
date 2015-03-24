8d69e5f7936a6ebafe3cf34798efcba37885836b
Julia <juhpassamani@hotmail.com>
2015-03-24 11:42:10 -0500

    Active Admin added in the project

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :users, ActiveAdmin::Devise.config
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   resources :games
 
   # The priority is based upon order of creation: first created -> highest priority.
