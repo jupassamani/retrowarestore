88d51a6ec521416528927260158cfd59e857a24a
Julia <juhpassamani@hotmail.com>
2015-03-30 11:08:10 -0500

    Generating an uploader

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,7 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-<<<<<<< HEAD
+
   devise :database_authenticatable, :registerable,
-=======
-  devise :database_authenticatable, 
->>>>>>> FETCH_HEAD
          :recoverable, :rememberable, :trackable, :validatable
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
+
 ActiveRecord::Schema.define(version: 20150324203908) do
-=======
-ActiveRecord::Schema.define(version: 20150324163914) do
->>>>>>> FETCH_HEAD
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string   "namespace",     limit: 255
