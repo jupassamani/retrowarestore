c8f61deb10378766f67fd0f6bb04750d43777b62
Julia <juhpassamani@hotmail.com>
2015-04-20 18:20:07 -0500

    Fixing some issues

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -14,4 +14,12 @@ class CategoriesController < ApplicationController
       @games = Game.where(category_id: @category.id)   
       @categories = Category.all
     end
+    
+    def cart_action(current_user_id)
+        if $redis.sismember "cart#{current_user_id}", id
+          'Remove from'
+        else
+          'Add to'
+        end
+    end
 end
